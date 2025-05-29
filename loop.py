f5ad845648fc3ce8042f65d86c3f798a62c5a64b
swawiye <sovereign.wawiye@gmail.com>
2025-05-29 08:33:36 +0300

    loop with an assigned list of numbers

---
+++ loop.py
@@ -0,0 +1,14 @@
+numbers = [24, 13, 57, 689, 9, 68, 72, 454, 32, 5, 6, 897, 8, 96, 54, 37, 26, 43, 25, 7, 68, 989, 6, 54, 73, 26, 243, 5, 76, 8]
+
+# Initializing the variables to the first element of the list of numbers
+min_value = numbers[0]
+max_value = numbers[0]
+
+for number in numbers:
+    if number < min_value:
+        min_value = number
+    if number > max_value:
+        max_value = number
+
+print("Smallest value:", min_value)
+print("Largest value:", max_value)
